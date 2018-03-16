21bc5dc32d13972a52c12c1de1c74f40fe4ffd74
jesus guzman <jessguzman044@gmail.com>
2018-03-16 02:07:45 -0600

    first commit

---
+++ contact_form.php
@@ -0,0 +1,66 @@
+<?php
+
+	$to = 'info@bodachera.com.mx';  // please change this email id
+	
+	$errors = array();
+	
+	// Check if email has been entered and is valid
+	if (!isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+		$errors['email'] = 'Please enter a valid email address';
+	}
+
+	$errorOutput = '';
+
+	if(!empty($errors)){
+
+		$errorOutput .= '<div class="alert alert-danger alert-dismissible" role="alert">';
+ 		$errorOutput .= '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+
+		$errorOutput  .= '<ul>';
+
+		foreach ($errors as $key => $value) {
+			$errorOutput .= '<li>'.$value.'</li>';
+		}
+
+		$errorOutput .= '</ul>';
+		$errorOutput .= '</div>';
+
+		echo $errorOutput;
+		die();
+	}
+
+
+
+	$name = 'nuevo usuario';
+	$email = $_POST['email'];
+	$message = 'Alguien se ha subscrito';
+	$from = $email;
+	$subject = 'Contact Form : Texas Lawers Responsive HTML5 Template';
+	
+	$body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+
+	//send the email
+	$result = '';
+	if (mail ($to, $subject, $body)) {
+		$result .= '<div class="alert alert-success alert-dismissible" role="alert">';
+ 		$result .= '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+		$result .= 'Thank You! I will be in touch';
+		$result .= '</div>';
+
+		echo $result;
+		die();
+	}
+
+	$result = '';
+	$result .= '<div class="alert alert-danger alert-dismissible" role="alert">';
+	$result .= '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+	$result .= 'Something bad happend during sending this message. Please try again later';
+	$result .= '</div>';
+
+	echo $result;
+	die();
+
+
+?>
+	
